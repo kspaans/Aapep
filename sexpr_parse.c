f6e97d11a1c0d911a0b7a6959045f7459e93172d
Kyle Spaans <kspaans@uwaterloo.ca>
2012-02-20 23:50:33 -0500

    hash table headers

--- sexpr_parse.c
+++ sexpr_parse.c
@@ -5,46 +5,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_NAME_LEN    32
-#define MAX_SYMBOL_LEN  32
-#define MAX_STRING_LEN 256
-
-struct atom;
-
-struct sexpr {
-	struct atom *first;
-	struct atom *rest;
-};
-
-enum atom_type {
-	EMPTY,
-	SEXPR,
-	NUM,
-	SYMBOL,
-	STRING,
-	NAME,   /* Must not start with a number, ', or ". */
-};
-
-struct atom {
-	enum atom_type atom_t;
-	union contents {
-		struct sexpr *sexp;
-		int           num;
-		const char   *symb;
-		char         *str;
-		const char   *name;
-	} contents;
-};
-
-enum parse_state {
-	SBOF,
-	SEOF,
-	SERROR,
-	SOPEN,
-	SCLOSE,
-
-};
+#include "sexpr_parse.h"
+#include "hash.h"
 
 /*
  * Names have enough exceptions that they are worth their own function, namely
